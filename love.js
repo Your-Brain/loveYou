5fd6fec6450e6976cece50f73ba0ab9eb375e5df
Your-Brain <144597205+Your-Brain@users.noreply.github.com>
2024-02-07 23:05:37 +0530

    Add files via upload
    lol

---
+++ love.js
@@ -0,0 +1,31 @@
+let img = document.querySelector("img");
+let input = document.querySelector(".input");
+
+let userInput = prompt("I LOVE YOU\n DO YOU LOVE ME ?");
+while (userInput === null) {
+    userInput = prompt("I LOVE YOU\n DO YOU LOVE ME ?");
+}
+
+// Responds to user input with appropriate text content
+switch (userInput) {
+    case "NO":
+    case "no":
+    case "No":
+        input.textContent = "Tato valo lagbena🫥";
+        break;
+    case "YES":
+    case "yes":
+    case "Yes":
+        input.textContent = "I love you too";
+        break;
+    case "MAYBE":
+    case "maybe":
+    case "Maybe":
+        input.textContent = "Let's see what happens";
+        break;
+    // Add more cases as needed
+    default:
+        input.textContent = "I don't understand your input";
+        break;
+}
+
